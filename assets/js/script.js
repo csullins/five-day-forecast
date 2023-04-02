8019b80e36c5bbdaec3b7b09fd616b58048b06a8
csullins <camilesullins@gmail.com>
2023-04-02 12:32:24 -0500

    fix storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,36 @@
 const apiKey = 'c5b0c54ba2eca16a585415f81e9d17e2'; 
 const cityButtons = document.querySelectorAll('.city-button');
 const tiles = document.querySelectorAll('.is-vertical');
-const mainTitle = document.querySelector('.is-3');
+const card = document.querySelector('.card');
+
+Storage.prototype.setObj = function(key, obj) {
+  return this.setItem(key, JSON.stringify(obj))
+}
+
+Storage.prototype.getObj = function(key) {
+  return JSON.parse(this.getItem(key))
+}
   
-// Check if a city has been previously selected and update the mainTitle accordingly
-if (localStorage.getItem('selectedCity')) {
-  mainTitle.textContent = localStorage.getItem('selectedCity');
+// Check if a city has been previously selected and update the cards/tiles accordingly
+let data = localStorage.getObj("dataCookie");
+
+if (data) {
+  populatePage(data);
 }
 
 cityButtons.forEach((button) => {
-  button.addEventListener('click', (e) => {
-    const city = e.target.textContent;
-    mainTitle.textContent = city;
-    // Save the selected city to localStorage
-    localStorage.setItem('selectedCity', city);
-
+  button.addEventListener('click', (event) => {
+    // button.classList.add('is-active');
+   const city = event.target.textContent;
+    // Enter the clicked city into the API call
     fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${city}&limit=5&appid=${apiKey}`)
     .then(response => response.json())
     .then(data => {
       let lat = data[0].lat;
       let lon = data[0].lon;
 
-      getWeather(lat,lon);
+  // Create lat, lon variables from the data and enter as params in the next API call
+    getWeather(lat,lon);
     })
     .catch(error => console.error(error));      
   });
@@ -31,43 +40,30 @@ getWeather = (lat, lon) => {
   fetch(`https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&appid=${apiKey}&units=imperial`)
   .then(response => response.json())
   .then(data => {
-      // Set the content of the first tile
-      const firstTileTemp = 'Temperature: ' + data.list[0].main.temp + ' F';
-      const firstTileWind = 'Wind: ' + data.list[0].wind.speed + ' MPH';
-      const firstTileHumidity = 'Humidity: ' + data.list[0].main.humidity + '%';
-      const ul = document.createElement('ul');
-      const tempLi = document.createElement('li');
-      tempLi.textContent = firstTileTemp;
-      const windLi = document.createElement('li');
-      windLi.textContent = firstTileWind;
-      const humidityLi = document.createElement('li');
-      humidityLi.textContent = firstTileHumidity;
-    
-      ul.appendChild(tempLi);
-      ul.appendChild(windLi);
-      ul.appendChild(humidityLi);
-      
-      mainTitle.appendChild(ul);
-      // Save the selected values to localStorage
-localStorage.setItem('temp', tempLi.textContent);
-localStorage.setItem('wind', windLi.textContent);
-localStorage.setItem('humidity', humidityLi.textContent);
-    
-
-    //set the content for 5-day tiles  
-    tiles.forEach((tile, index) => {
-      tile.querySelector('li:nth-child(1)').textContent = 'Temperature: ' + data.list[index * 8].main.temp +' F';
-      
-      tile.querySelector('li:nth-child(2)').textContent = 'Wind: ' + data.list[index * 8].wind.speed +' MPH';
-      tile.querySelector('li:nth-child(3)').textContent = 'Humidity: ' + data.list[index * 8].main.humidity + '%';
-      
-      const dateStr = data.list[index * 8].dt_txt;
-      const date = new Date(dateStr);
-      const formattedDate = date.toLocaleDateString('en-US');
-
-      tile.querySelector('.title').textContent = formattedDate;
-  
-    })
-  })
+    populatePage(data);
+    localStorage.setObj("dataCookie", data);
+      }) 
   .catch(error => console.error(error));
 };
+
+function populatePage(data) {
+        // Set the content for the hero tile    
+        card.querySelector('li:nth-child(1)').textContent = 'Temp: '+ data.list[0].main.temp +' F';
+        card.querySelector('li:nth-child(2)').textContent= 'Wind: ' + data.list[0].wind.speed +' MPH';
+        card.querySelector('li:nth-child(3)').textContent = 'Humidity: ' + data.list[0].main.humidity + '%';
+        
+      //set the content for 5-day tiles  
+      tiles.forEach((tile, index) => {
+        tile.querySelector('li:nth-child(1)').textContent = 'Temp: ' + data.list[index * 8].main.temp +' F';
+        tile.querySelector('li:nth-child(2)').textContent = 'Wind: ' + data.list[index * 8].wind.speed +' MPH';
+        tile.querySelector('li:nth-child(3)').textContent = 'Humidity: ' + data.list[index * 8].main.humidity + '%';
+        
+        const dateStr = data.list[index * 8].dt_txt;
+        const date = new Date(dateStr);
+        const formattedDate = date.toLocaleDateString('en-US');
+
+        tile.querySelector('.subtitle').textContent = formattedDate;
+        card.querySelector('.title').textContent = data.city.name;
+  })
+};
+
